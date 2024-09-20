365e57197b393728d692b72a23b7b297a2343028
Aryan Dubey <aryanbaba@gmail.com>
2024-09-20 15:03:02 +0530

    User section completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { API_URL } from "./Api";
+import axios from "axios";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Box,
+  Dialog,
+  Typography,
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const defaultFormData = {
+  name: "",
+  email: "",
+  age: "",
+};
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [formData, setFormData] = useState(defaultFormData);
+
+  useEffect(() => {
+    getUsersDetails();
+  }, []);
+
+  const getUsersDetails = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/users/getUsers`);
+      if (res.status === 200) {
+        setUsers(res.data);
+      } else {
+        console.log(res.status);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const userKeys = Object.keys(users.length > 0 ? users[0] : {}).filter(
+    (key) => key !== "_id" && key !== "__v"
   );
-}
+
+  return (
+    <>
+      {users.length === 0 ? (
+        <Box>
+          <Typography>User Not Found</Typography>
+        </Box>
+      ) : (
+        <>
+          <Typography
+            align="center"
+            marginY={2}
+            fontSize={20}
+            fontWeight={600}
+            color="secondary"
+          >
+            Users
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {userKeys.map((key) => (
+                    <TableCell align="center" key={key}>
+                      {key === "_id" && key === "__v" ? "" : key}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map((user) => (
+                  <TableRow key={user._id}>
+                    {Object.keys(user)
+                      .filter((key) => key !== "_id" && key !== "__v")
+                      .map((key) => (
+                        <TableCell align="center" key={key}>
+                          {user[key]}
+                        </TableCell>
+                      ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+      <Box
+        display={"flex"}
+        alignContent={"center"}
+        justifyContent={"center"}
+        marginTop={5}
+      >
+        <Button variant="contained" onClick={() => setOpen(true)}>
+          Create User
+        </Button>
+      </Box>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <Box component="form" onSubmit={handleSubmit} padding={5}>
+          <Typography fontSize={20} align="center">
+            Enter user information
+          </Typography>
+          <FormControl fullWidth margin="normal">
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <OutlinedInput
+              id="name"
+              name="name"
+              label="Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <OutlinedInput
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl fullWidth margin="normal">
+            <InputLabel htmlFor="age">Age</InputLabel>
+            <OutlinedInput
+              id="age"
+              name="age"
+              type="number"
+              label="Age"
+              value={formData.age}
+              onChange={handleChange}
+            />
+          </FormControl>
+          <Box display={"flex"} justifyContent={"center"} marginTop={3}>
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Dialog>
+    </>
+  );
+};
 
 export default App;
